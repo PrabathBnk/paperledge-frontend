a14ca93b081570b9b7e5dc426d60adc5a749fbfe
PrabathBnk <prabathbanukada@gmail.com>
2024-11-12 22:22:32 +0530

    Load User Date to User Details Section

--- src/app/pages/dashboard/user-details/user-details.component.ts
+++ src/app/pages/dashboard/user-details/user-details.component.ts
@@ -1,12 +1,32 @@
 import { NgFor } from '@angular/common';
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-user-details',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './user-details.component.html',
   styleUrl: './user-details.component.css'
 })
-export class UserDetailsComponent {
+export class UserDetailsComponent implements OnInit{
+  public user:any;
+  public inprogessOrders:any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.user = localStorage.getItem("user");
+    this.user = JSON.parse(this.user);
+
+    this.loadInProgressOrders();
+  }
+
+  loadInProgressOrders() {
+    this.http.get<any[]>(`http://localhost:8080/order/all/by-user-id?id=${this.user.id}`).subscribe(res => {
+      this.inprogessOrders = res;
+
+      this.inprogessOrders = this.inprogessOrders.filter(order =>  order.status.name != 'Returned' && order.status.name != 'Cancelled');
+    });
+  }
 }

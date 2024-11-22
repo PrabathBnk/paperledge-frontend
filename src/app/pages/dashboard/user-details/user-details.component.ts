30de1f324528aa6167c41180267344d89d2a6e50
PrabathBnk <prabathbanukada@gmail.com>
2024-11-22 21:23:32 +0530

    Profile Picture Feature

--- src/app/pages/dashboard/user-details/user-details.component.ts
+++ src/app/pages/dashboard/user-details/user-details.component.ts
@@ -30,4 +30,35 @@ export class UserDetailsComponent implements OnInit{
       this.inprogessOrders = this.inprogessOrders.filter(order =>  order.status.name != 'Returned' && order.status.name != 'Cancelled' && order.status.name != 'Delivered');
     });
   }
+
+  updateProfilePictue(event:Event){
+    if((event.target as HTMLInputElement).files == null) {
+      alert("Something went wrong!");
+      return;
+    } 
+    
+    const input = event.target as HTMLInputElement;
+    let imageFile = input.files?.item(0);
+
+    if (imageFile != null && (imageFile.size/1024) > 5120) {
+      alert("File should be less than 5MB");
+      return;
+    }
+
+    if(imageFile != null){
+      const formData = new FormData();
+      formData.append("id", "PL20240003");
+      formData.append("image", imageFile);
+      
+      this.http.put<any>("http://localhost:8080/user/profile-picture", formData).subscribe({
+        next: (res) => {
+          this.user.profilePicture = res.image;
+          localStorage.setItem('user', JSON.stringify(this.user));
+        }, 
+        error: (error) => {
+          alert("Something went wrong");
+        }
+      });
+    }
+  }
 }
